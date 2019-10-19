0af46bdbbc7f57037e49f274617f1d50cddadd8c
stanley <stanleygomesdasilva@gmail.com>
2019-10-19 17:33:48 -0300

    docker config files

---
+++ docker/migrations/V001__init_db.sql
@@ -0,0 +1,5 @@
+-- create database nodetello;
+-- create user 'nodetello'@'localhost' identified by 'nodetello';
+-- grant all privileges on * . * to 'nodetello'@'localhost';
+-- flush privileges;
+
