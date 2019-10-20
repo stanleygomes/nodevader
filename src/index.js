0df688c34b5df1098d5385fbc8e1bf815c201b7d
stanley <stanleygomesdasilva@gmail.com>
2019-10-20 17:02:16 -0300

    adjustments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var findup = require('findup-sync')
 var config = require(findup('config.json'))
-const router = require('./routes')
+const routes = require('./routes')
 const moment = require('moment')
 
 const app = express()
@@ -9,5 +9,5 @@ const baseEndpoint = config.baseEndpoint
 const port = config.server.port
 const timeStart = moment().format('DD/MM/YYYY HH:mm')
 
-app.use(baseEndpoint, router)
+app.use(baseEndpoint, routes)
 app.listen(port, () => console.log(`Running on port ${port}. Started at: ${timeStart}`))
--- src/resource/helloWorldResource.js
+++ src/resource/helloWorldResource.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const helloWorldResource = express.Router()
 const http = require('../utils/http')
-const helloWorldService = require('../service/helloWorldService')
+// const helloWorldService = require('../service/helloWorldService')
 
 helloWorldResource.get('', (req, res) => {
   http.httpJson(res, {
-    'Hello': 'World'
+    message: 'Hello World'
   })
   // helloWorldService.helloWorld(req, res, http.callbackResponse(res))
 })
