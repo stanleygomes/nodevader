8c238fcb40cef218b2bd3cedd33160e06e66fcab
stanley <stanleygomesdasilva@gmail.com>
2019-10-20 17:02:16 -0300

    adjustments

--- src/service/helloWorldService.js
+++ src/service/helloWorldService.js
@@ -1,5 +1,5 @@
 const helloWorld = (req, res, callback) => {
-  callback('Hello World')
+  // callback('Hello World')
 }
 
 module.exports = {
--- src/test/helloWorldTest.js
+++ src/test/helloWorldTest.js
@@ -1,10 +1,10 @@
-const chai = require('chai')
-const helloWorldService = require('../service/helloWorldService')
+// const chai = require('chai')
+// const helloWorldService = require('../service/helloWorldService')
 
-const expect = chai.expect
+// const expect = chai.expect
 
-describe('Teste de serviço de helloWorld', () => {
-  it('Validar retorno de helloWorld', () => {
-    expect(helloWorldService.show).to.have.lengthOf.above(0)
-  })
-})
+// describe('Teste de serviço de helloWorld', () => {
+//   it('Validar retorno de helloWorld', () => {
+//     expect(helloWorldService.show).to.have.lengthOf.above(0)
+//   })
+// })
