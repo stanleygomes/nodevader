1e9d2198f4866bc89e3c05c29238114beaa124be
Stanley Gomes <stanleygomesdasilva@gmail.com>
2019-10-22 16:13:02 -0300

    add security layer with helmet

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ var config = require(findup('config.json'))
 const routes = require('./routes')
 const moment = require('moment')
 const cors = require('cors')
+const helmet = require('helmet')
 
 const app = express()
 const timeStart = moment().format('DD/MM/YYYY HH:mm')
 
 app.use(cors(config.cors))
+app.use(helmet())
 app.use(config.baseEndpoint, routes)
 app.listen(config.server.port, () => console.log(`Running on port ${config.server.port}. Started at: ${timeStart}`))
