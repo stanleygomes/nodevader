e1bcfadc57ee75e344fdcc569aeadc04e573422b
Stanley <stanleygomesdasilva@gmail.com>
2019-10-23 18:01:55 -0300

    starting smtp functionality

--- src/api/helloWorld/helloWorldRest.js
+++ src/api/helloWorld/helloWorldRest.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const helloWorldRest = express.Router()
 const httpResponse = require('../../utils/httpResponse')
+const smtp = require('../../utils/smtp')
 // const helloWorldService = require('../service/helloWorldService')
 
 helloWorldRest.get('', (req, res) => {
+  smtp.main().catch(console.error)
   httpResponse.json(res, {
     message: 'Hello World'
   })
