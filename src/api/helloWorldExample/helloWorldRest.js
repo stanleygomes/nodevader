ed9e70d8d9e3f58ee3241b0fcb34bf92da04d6c5
Stanley <stanleygomesdasilva@gmail.com>
2019-10-25 14:53:23 -0300

    starting send

--- src/api/helloWorldExample/helloWorldRest.js
+++ src/api/helloWorldExample/helloWorldRest.js
@@ -1,23 +1,13 @@
 const express = require('express')
 const helloWorldRest = express.Router()
 const httpResponse = require('../../utils/httpResponse')
-<<<<<<< HEAD:src/api/helloWorld/helloWorldRest.js
 const smtp = require('../../utils/smtp')
-// const helloWorldService = require('../service/helloWorldService')
-
-helloWorldRest.get('', (req, res) => {
-  smtp.main().catch(console.error)
-  httpResponse.json(res, {
-    message: 'Hello World'
-  })
-
-  // helloWorldService.helloWorld(req, res)
-=======
 const helloWorldService = require('./helloWorldService')
 
 helloWorldRest.get('', (req, res) => {
+  smtp.sendMail()
+  // smtp.main().catch(console.error)
   httpResponse.json(res, helloWorldService.helloWorld())
->>>>>>> master:src/api/helloWorldExample/helloWorldRest.js
 })
 
 module.exports = helloWorldRest
