5cdc255209b2892fb5b6d1fb03d84a516d91ea35
Stanley <stanleygomesdasilva@gmail.com>
2019-10-29 15:06:35 -0300

    request through servers using axios

--- src/api/helloWorldExample/helloWorldRest.js
+++ src/api/helloWorldExample/helloWorldRest.js
@@ -3,8 +3,19 @@ const helloWorldRest = express.Router()
 const httpResponse = require('../../utils/httpResponse')
 const helloWorldService = require('./helloWorldService')
 
+const fs = require('fs')
+
+const httpRequest = require('../../utils/httpRequest')
+
 helloWorldRest.get('', (req, res) => {
-  httpResponse.json(res, helloWorldService.helloWorld())
+  httpRequest.post({}).then((response) => {
+    response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+    console.log('baixou')
+    httpResponse.json(res, helloWorldService.helloWorld())
+  }).catch((error) => {
+    httpResponse.error(res, 'error')
+    console.log(error)
+  })
 })
 
 module.exports = helloWorldRest
---
+++ src/utils/httpRequest.js
@@ -0,0 +1,40 @@
+const axios = require('axios')
+const joi = require('@hapi/joi')
+
+const schema = joi.object().keys({
+  method: joi.string(),
+  url: joi.string().required(),
+  responseType: joi.string().required()
+})
+
+const post = (params) => {
+  return new Promise((resolve, reject) => {
+    const result = joi.validate(params, schema)
+
+    if (!result) {
+      const error = new Error('Invalid parameters')
+      reject(error)
+    }
+
+    const defaultParams = {
+      baseURL: '',
+      method: 'post',
+      url: '',
+      headers: { 'X-Requested-With': 'XMLHttpRequest' },
+      params: {},
+      data: {},
+      timeout: 1000,
+      responseType: 'json'
+    }
+
+    axios({ ...defaultParams, ...params }).then((response) => {
+      resolve(response)
+    }).catch((error) => {
+      reject(error)
+    })
+  })
+}
+
+module.exports = {
+  post
+}
