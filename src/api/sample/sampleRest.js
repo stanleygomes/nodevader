986c31268b2b96152033076fccc2869e6fc72d75
Stanley <stanleygomesdasilva@gmail.com>
2019-10-30 13:04:04 -0300

    move http request example to api

--- src/api/sample/sampleRest.js
+++ src/api/sample/sampleRest.js
@@ -3,6 +3,7 @@ const sampleRest = express.Router()
 const httpResponse = require('../../utils/httpResponse')
 const sampleService = require('./sampleService')
 const logger = require('../../utils/logger')
+const httpRequest = require('../../utils/httpRequest')
 
 sampleRest.get('/logger', (req, res) => {
   logger.error('Error!!')
@@ -15,6 +16,20 @@ sampleRest.get('/logger', (req, res) => {
 sampleRest.get('/helloWorld', (req, res) => {
   sampleService.helloWorld().then((response) => {
     httpResponse.json(res, response)
+  })
+})
+
+sampleRest.get('/httpRequest', (req, res) => {
+  const params = {
+    url: 'https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg'
+  }
+
+  httpRequest.get(params).then((response) => {
+    console.log('OK')
+    httpResponse.json(res, response)
+  }).catch((error) => {
+    console.log('error')
+    httpResponse.error(res, error)
   })
 })
 
