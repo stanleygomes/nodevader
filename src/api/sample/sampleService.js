0f39b6816e7328283aeb033d3c242063d0646c61
Stanley <stanleygomesdasilva@gmail.com>
2019-10-30 14:06:56 -0300

    adjustment

--- src/api/sample/sampleService.js
+++ src/api/sample/sampleService.js
@@ -5,7 +5,7 @@ const joi = require('@hapi/joi')
 const schema = joi.object().keys({
   username: joi.string().alphanum().min(3).max(30).required(),
   password: joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/),
-  repeat_password: joi.ref('password'),
+  repeat_password: joi.ref('password')
 })
 
 const helloWorld = () => {
