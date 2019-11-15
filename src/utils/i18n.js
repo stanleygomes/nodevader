9183b5140da4119bacae842aaf472f934613180b
stanley <stanleygomesdasilva@gmail.com>
2019-11-15 20:03:24 -0300

    fix path resolve for i18n

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,7 +1,7 @@
 const i18n = require('i18n')
 const config = require('../config.json')
 const i18nConfig = config.i18n
-i18nConfig.directory = __dirname + config.i18n.directory
+i18nConfig.directory = path.join(__dirname, config.i18n.directory)
 
 i18n.configure(i18nConfig)
 i18n.translate = (text, ...parameters) => i18n.__(text, ...parameters)
