265531c8e3b033741e5e6b157c7332368d5c71f2
Stanley <stanleygomes@softbox.com.br>
2020-02-05 11:30:59 -0300

    remove sample routes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const express = require('./express')
+const started = () => console.log(`Running on port ${express.config.port}. Started at: ${express.time}.`)
 
-express.app.listen(express.config.port, () => console.log(`Running on port ${express.config.port}. Started at: ${express.time}`))
+express.app.listen(express.config.port, started)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const sampleRest = require('../api/sample/sampleRest')
 const authRest = require('../api/auth/authRest')
 const protectedRoutesRest = require('../api/auth/protectedRoutesRest')
 const authMiddleware = require('../middlewares/authMiddleware')
@@ -9,7 +8,6 @@ const loggerUtils = require('../utils/logger')
 
 /* non auth routes */
 router.use('/auth', authRest)
-router.use('/sample', sampleRest)
 
 /* auth routes */
 router.use(authMiddleware)
