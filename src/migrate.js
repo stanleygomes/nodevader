f23c216693a4f2cb6287bebf34c350c4d7653387
Stanley <stanleygomesdasilva@gmail.com>
2020-07-21 00:16:31 -0300

    node migrations with postgrator

---
+++ src/migrate.js
@@ -0,0 +1,28 @@
+const Postgrator = require('postgrator')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const postgrator = new Postgrator({
+  // Directory containing migration files
+  migrationDirectory: `${__dirname}/migrations/`,
+  // or a glob pattern to files
+  // migrationPattern: __dirname + '/migrations/*',
+  // Driver: must be pg, mysql, mysql2 or mssql
+  driver: process.env.DB_CLIENT,
+  // Database connection config
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_USERNAME,
+  username: process.env.DB_PASSWORD,
+  password: process.env.DB_DATABASE,
+  // Schema table name. Optional. Default is schemaversion
+  // If using Postgres, schema may be specified using . separator
+  // For example, { schemaTable: 'schema_name.table_name' }
+  schemaTable: 'schema_version'
+})
+
+// Migrate to max version (optionally can provide 'max')
+postgrator
+  .migrate()
+  .then((appliedMigrations) => console.log(appliedMigrations))
+  .catch((error) => console.log(error))
---
+++ src/migrations/V001__init_db.sql
@@ -0,0 +1,5 @@
+-- create database nodevader;
+-- create user 'nodevader'@'localhost' identified by 'nodevader';
+-- grant all privileges on * . * to 'nodevader'@'localhost';
+-- flush privileges;
+
